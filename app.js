4b225eca1a519fb04873ae41ec78d056e43d686e
Eugene Cheung <echeung@infusion.com>
2016-01-31 14:54:31 -0500

    Minor cleanup, remove Bootstrap

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var express        = require('express');
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+var bodyParser     = require('body-parser'),
+    express        = require('express'),
+    methodOverride = require('method-override'),
+    mongoose       = require('mongoose');
 
 var app = module.exports.app = exports.app = express();
 
 
 // Configuration ===========================================
-
 var port = process.env.PORT || 8080;
 mongoose.connect(process.env.SUSHIDB);
 
 // Get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 // For livereload
 app.use(require('connect-livereload')());
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+// Override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// Set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
 
 
 // Routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+require('./app/routes')(app); // Pass our application into our routes
+
 
 // Start app ===============================================
 app.listen(port);
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+console.log('Server started on port', port);
