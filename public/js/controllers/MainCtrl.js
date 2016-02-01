b2fc8ea82a20b14599f251fbc18718a28f42fb08
Eugene Cheung <echeung@infusion.com>
2016-01-31 21:00:12 -0500

    Clean up header markup/styling

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,37 +1,35 @@
 app.controller('MainCtrl', ['$scope', function ($scope) {
 
-    function init () {
-        $scope.nav = {
-            tabs: [
-                {
-                    path: '',
-                    value: 'Home'
-                },
-                {
-                    path: 'players',
-                    value: 'Players'
-                },
-                {
-                    path: 'admin',
-                    value: 'Admin'
-                }
-            ],
-            active: 0
-        };
+    $scope.nav = {
+        tabs: [
+            {
+                path: '',
+                title: 'Home'
+            },
+            {
+                path: 'players',
+                title: 'Players'
+            },
+            {
+                path: 'admin',
+                title: 'Admin'
+            }
+        ],
+        active: 0
+    };
 
+    $scope.changeTab = function (index) {
+        $scope.nav.active = index;
+    };
+
+    (function init () {
         var pathname = window.location.pathname.slice(1);
-        for (var i = 0; i < $scope.nav.tabs.length; i++) {
-            if (pathname == $scope.nav.tabs[i].path) {
+        for (var i = 0, len = $scope.nav.tabs.length; i < len; i++) {
+            if (pathname === $scope.nav.tabs[i].path) {
                 $scope.nav.active = i;
                 return;
             }
         }
-    };
-
-    init();
-
-    $scope.changeTab = function(e, index){
-        $scope.nav.active = index;
-    };
+    })();
 
 }]);
