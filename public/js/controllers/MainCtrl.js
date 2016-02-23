e3c93d9278673e019161390c9d66b632ffe062dd
Eugene Cheung <echeung@infusion.com>
2016-02-23 09:24:46 -0500

    Move admin link to footer, use a directive for nav active tab

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,37 +1,37 @@
 app.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.nav = {
-    tabs: [
-      {
-        path: '',
-        title: 'Home'
-      },
-      {
-        path: 'games',
-        title: 'Games'
-      },
-      {
-        path: 'players',
-        title: 'Players'
-      },
-      {
-        path: 'admin',
-        title: 'Admin'
-      }
-    ],
-    active: 0
-  };
-
-  $scope.changeTab = function (index) {
-    $scope.nav.active = index;
-  };
-
-  (function () {
-    var pathname = window.location.pathname.slice(1);
-    for (var i = 0, len = $scope.nav.tabs.length; i < len; i++) {
-      if (pathname === $scope.nav.tabs[i].path) {
-        $scope.nav.active = i;
-        return;
-      }
+  $scope.nav = [
+    {
+      path: '',
+      title: 'Home'
+    },
+    {
+      path: 'games',
+      title: 'Games'
+    },
+    {
+      path: 'players',
+      title: 'Players'
     }
-  })();
+  ];
+}]);
+
+app.directive('autoNavActive', ['$location', function ($location) {
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function (scope, element) {
+      function setActive() {
+        var path = $location.path();
+
+        if (path) {
+          angular.forEach(element.find('a'), function (anchor) {
+            angular.element(anchor).toggleClass('header__nav__item--active', anchor.href.match(path + '(?=\\?|$)'));
+          });
+        }
+      }
+
+      setActive();
+      scope.$on('$locationChangeSuccess', setActive);
+    }
+  };
 }]);
